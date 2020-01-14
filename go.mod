66edfbd015416a68bd28193049c470ad189406ff
Will Dixon <will@acst.com>
2020-01-13 23:17:21 -0500

    Load packages into a context

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.13
 require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/hashicorp/hcl/v2 v2.3.0
+	github.com/sirkon/goproxy v1.4.8
 	github.com/zclconf/go-cty v1.2.0
+	golang.org/x/tools v0.0.0-20200114015519-9dc88fb721c3
 )
--- main.go
+++ main.go
@@ -3,19 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"path"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/gohcl"
-	"github.com/hashicorp/hcl/v2/hclparse"
 )
 
 var dir string
 
-var ctx *hcl.EvalContext = &hcl.EvalContext{
+var hclCtx *hcl.EvalContext = &hcl.EvalContext{
 	Variables: joinCtyVariables(stringGoTypeCty, stringConverterTypeCty),
 	Functions: stringFuncTypeCty,
 }
@@ -30,12 +26,17 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	files, err := parseHCLFiles()
+	ctx, err := newCtx(dir)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	spew.Dump(files)
+	err = load(ctx)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	spew.Dump(ctx)
 }
 
 func parseFlags() error {
@@ -48,32 +49,4 @@ func parseFlags() error {
 		dir = cwd
 	}
 	return nil
-}
-
-func parseHCLFiles() ([]*File, error) {
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return nil, fmt.Errorf("unable to read directory: %w", err)
-	}
-	parser := hclparse.NewParser()
-	var hclFiles []*File
-	for _, f := range files {
-		if f.IsDir() {
-			continue
-		}
-		if path.Ext(f.Name()) != ".hcl" {
-			continue
-		}
-		hcl, diag := parser.ParseHCLFile(path.Join(dir, f.Name()))
-		if diag.HasErrors() {
-			return nil, fmt.Errorf("unable to parse %s: %w", f.Name(), diag)
-		}
-		var config File
-		diag = gohcl.DecodeBody(hcl.Body, ctx, &config)
-		if diag.HasErrors() {
-			return nil, fmt.Errorf("unable to decode into go struct: %w", diag)
-		}
-		hclFiles = append(hclFiles, &config)
-	}
-	return hclFiles, nil
 }
